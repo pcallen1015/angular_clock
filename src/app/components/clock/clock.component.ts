1d3e02ca7282d1ddf0a7c4260af1eb0d418cdc15
Colin Allen <colallen@cisco.com>
2019-05-08 12:00:40 -0700

    Hour, minute, second hand all have basic functionality with incremental movement between tick marks.

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ClockComponent implements OnInit {
   Arr = Array;
+  Math = Math;
 
   public hours: number;
   public minutes: number;
